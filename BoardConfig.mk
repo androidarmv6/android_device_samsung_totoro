e1ebba1539879e661905c79bb869f3861cc30f89
Conn O'Griofa <connogriofa@gmail.com>
2014-06-03 22:37:49 +0100

    totoro: initialize Bluedroid configuration
    Change-Id: I8bde3c141704a7e21d1b0b62c96579016409ab24

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,9 @@ LOCAL_PATH:= $(call my-dir)
 
 include device/samsung/bcm21553-common/BoardConfigCommon.mk
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/totoro/bluetooth
+
 # Board
 TARGET_BOARD_PLATFORM				:= bcm21553
 TARGET_BOOTLOADER_BOARD_NAME 			:= totoro
